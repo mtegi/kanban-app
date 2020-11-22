d886cbcb1960ac60965e18393c35a0bcc4541b8f
Michał Tęgi <216906@edu.p.lodz.pl>
2020-11-22 19:12:32 +0100

    favourite tooltip

--- webapp/src/components/boards/main/menu/FavoriteButton.js
+++ webapp/src/components/boards/main/menu/FavoriteButton.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Favorite, FavoriteBorder } from '@material-ui/icons';
 import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/core/styles';
+import { Tooltip } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 const StyledIconButton = withStyles(() => ({
   root: {
@@ -12,11 +14,16 @@ const StyledIconButton = withStyles(() => ({
   },
 }))(IconButton);
 
-const FavoriteButton = ({ isFavourite, onClick }) => (
-  <StyledIconButton color="secondary" onClick={onClick}>
-    {isFavourite ? <Favorite /> : <FavoriteBorder />}
-  </StyledIconButton>
-);
+const FavoriteButton = ({ isFavourite, onClick }) => {
+  const { t } = useTranslation('boards');
+  return (
+    <Tooltip title={t('add-to-favourite')} placement="top-center">
+      <StyledIconButton color="secondary" onClick={onClick}>
+        {isFavourite ? <Favorite /> : <FavoriteBorder />}
+      </StyledIconButton>
+    </Tooltip>
+  );
+};
 
 FavoriteButton.propTypes = {
   isFavourite: PropTypes.bool.isRequired,
